b94012b9f036a57a565a5fc4dcd4ea407845d22c
christian bianchini <max246@gmail.com>
2026-03-19 17:26:05 +0000

    fix hasing

--- backend/utils/hash_password.py
+++ backend/utils/hash_password.py
@@ -10,4 +10,7 @@ if len(sys.argv) != 2:
     print("Usage: python utils/hash_password.py <password>")
     sys.exit(1)
 
-print(pwd_context.hash(sys.argv[1]))
+hashed = pwd_context.hash(sys.argv[1])
+env_safe = hashed.replace("$", "$$")
+print(f"Hash:     {hashed}")
+print(f"For .env: SUPERUSER_PASSWORD={env_safe}")
