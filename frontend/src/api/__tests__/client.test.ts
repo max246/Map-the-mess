9c81fd17be25a661e71d81719116b3ecb1088dc0
christian bianchini <max246@gmail.com>
2026-04-13 08:54:14 +0100

    fix

--- frontend/src/api/__tests__/client.test.ts
+++ frontend/src/api/__tests__/client.test.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
+// eslint-disable-next-line no-var -- var required: jest.mock() is hoisted above let/const
 var mockOnResponseError: any
 
 jest.mock('axios', () => {
